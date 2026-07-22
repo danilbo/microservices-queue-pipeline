412705b4878bd6b4cbc63fcf0eda6cf384b6b456
danilbo <danilbo99@gmail.com>
2026-07-22 18:09:30 +0300

    gid

--- jenkins/agent/entrypoint.sh
+++ jenkins/agent/entrypoint.sh
@@ -6,9 +6,13 @@ chown -R jenkins:jenkins /home/jenkins/.ssh
 chmod 600 /home/jenkins/.ssh/authorized_keys
 
 DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
-if ! getent group docker > /dev/null; then
+DOCKER_GROUP=$(getent group "$DOCKER_GID" | cut -d: -f1)
+
+if [ -z "$DOCKER_GROUP" ]; then
     groupadd -g "$DOCKER_GID" docker
+    DOCKER_GROUP=docker
 fi
-usermod -aG docker jenkins
+
+usermod -aG "$DOCKER_GROUP" jenkins
 
 exec /usr/sbin/sshd -D
